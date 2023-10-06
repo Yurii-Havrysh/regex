0ad7b926a460a20fe50713a550ddd97296606c99
Yurii Havrysh <gavrisch.yuri@gmail.com>
2023-10-06 17:59:01 +0200

    second commit

--- index.js
+++ index.js
@@ -1,85 +1,102 @@
 function myJSONParse(jsonString) {
-//creating regEx (pattern which helps me find string and parse it into json object)
-    const regex = /(".*?"|\d+|\[|\]|\{|\}|\:|\,|true|false|null)/g; 
-    const tokens = jsonString.match(regex); 
-    let index = 0;
-   
-    function parseValue() {
-        const token = tokens[index++];
-//algorithms that parse objects
-        if (token === "{") {
-            const obj = {};
-            let key = parseString();
-            while (tokens[index] !== "}") {
-                expect(":");
-                obj[key] = parseValue();
-                if (tokens[index] === ",") {
-                    index++;
-                    key = parseString();
+    const stack = [];
+    let currentObject = null;
+    let key = null;
+
+    const regex = /(".*?"|\d+|\[|\]|\{|\}|\:|\,|true|false|null)/g;
+    const tokens = jsonString.match(regex) || [];
+
+    tokens.forEach(token => {
+        if (token === '{') {
+            if (currentObject) {
+                stack.push(currentObject);
+                stack.push(key);
+            }
+            currentObject = {};
+            key = null;
+        } else if (token === '}') {
+            const prevKey = stack.pop();
+            const prevObject = stack.pop();
+
+            if (prevObject instanceof Array) {
+                prevObject.push(currentObject);
+                currentObject = prevObject;
+            } else if (prevObject) {
+                prevObject[prevKey] = currentObject;
+                currentObject = prevObject;
+            }
+        } else if (token === '[') {
+            if (currentObject) {
+                stack.push(currentObject);
+                stack.push(key);
+            }
+            currentObject = [];
+            key = null;
+        } else if (token === ']') {
+            const prevKey = stack.pop();
+            const prevObject = stack.pop();
+
+            if (prevObject) {
+                prevObject[prevKey] = currentObject;
+                currentObject = prevObject;
+            }
+        } else if (token === ':') {
+        } else if (token === ',') {
+        } else {
+            if (key === null) {
+                key = JSON.parse(token);
+            } else {
+                const value = JSON.parse(token);
+                if (currentObject instanceof Array) {
+                    currentObject.push(value);
+                } else if (key) {
+                    currentObject[key] = value;
+                    key = null;
                 }
             }
-            index++; 
-            return obj;
         }
-//algorithms that parse arrays
-        if (token === "[") {
-            const arr = [];
-            while (tokens[index] !== "]") {
-                arr.push(parseValue());
-                if (tokens[index] === ",") {
-                    index++;
-                }
-            }
-            index++; 
-            return arr;
-        } 
-//conditions for different data types
-        if (/^"\S*"$/.test(token)) {
-            return token.slice(1, -1);
-        }
+    });
 
-        if (/^true$/.test(token)) {
-            return true;
-        }
-
-        if (/^false$/.test(token)) {
-            return false;
-        }
-
-        if (/^null$/.test(token)) {
-            return null;
-        }
-
-        if (/^-?\d*\.?\d+(?:[eE][-+]?\d+)?$/.test(token)) {
-            return parseFloat(token);
-        }
-        throw new SyntaxError("Unexpected token: " + token); //error handle
-    } 
-//creating string parse function
-    function parseString() {
-        const token = tokens[index++];
-        if (/^"\S*"$/.test(token)) {
-            return token.slice(1, -1);
-        }
-        throw new SyntaxError("Invalid string: " + token); //error handle
-    }
-//expected tokens. If token is suitable, than we continue walk through array. 
-//If not, there is error pointing to unexpected token
-    function expect(expectedToken) {
-        const token = tokens[index++];
-        if (token !== expectedToken) {
-            throw new SyntaxError("Expected " + expectedToken + " but found " + token); 
-        } ////error handle
+    if (currentObject === null) {
+        throw new SyntaxError('Unexpected end of input');
     }
 
-    return parseValue();
+    return currentObject;
 }
-//testing
-const jsonString = '{"name": "Dariusz", "surname": "Zieliński", "age": 46, "city": "Gdynia", "married": true}';
-const jsonObject = myJSONParse(jsonString);
 
-console.log(jsonObject);
+const jsonString = `{
+    "id": "647ceaf3657eade56f8224eb",
+    "index": 10,
+    "negativeIndex": -10,
+    "anEmptyArray": [],
+    "notEmptyArray": [1, 2, 3,"string", true, null],
+    "boolean": true,
+    "nullValue": null,
+    "nestedObject": {
+        "nestedString": "Hello World",
+        "nestedNumber": 42,
+        "nestedArray": [true, false]
+    },
+    "complexArray": [
+        {
+            "name": "Alice Alice",
+            "age": 28,
+            "hobbies": ["Reading", "Painting"]
+        },
+        {
+            "name": "Bob Bob",
+            "age": 32,
+            "hobbies": ["Gaming", "Cooking"]
+        }
+    ]
+}`;
 
+try {
+    const jsonObject = myJSONParse(jsonString);
+    console.log(jsonObject);
+} catch (error) {
+    console.error(error.message);
+}
 
 
 
